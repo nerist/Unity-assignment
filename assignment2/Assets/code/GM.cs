10d75df1571cb68e49f3050c5b8561173755c184
Nicholas Farrugia <Nerist@Nicholass-MacBook-Pro.local>
2015-06-15 00:00:42 +0200

    Added second level
    *Added second level
    *Added score and GUI script

---
+++ assignment2/Assets/code/GM.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GM : MonoBehaviour {
+	public static int score;
+	public static int lives;
+	public GUISkin stylesheet;
+	// Use this for initialization
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	void OnGUI ()
+	{
+		//to load stylesheet
+		GUI.skin = stylesheet;
+		string ScoreLabel = "Score:";
+		string LivesLabel = "Lives:";
+		//(posx,posy,width,height)
+		GUI.Label (new Rect (10f, 10f, 150, 20), ScoreLabel + score);
+		//created space by adding 15px of spaceing
+		GUI.Label (new Rect (Screen.width - 80f, 10f, 150, 20), LivesLabel + lives);
+	}
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
