0a3e109a9316169f330651f0524bf4a1a59c928b
Nicholas Farrugia <Nerist@Nicholass-MacBook-Pro.local>
2015-06-15 00:23:40 +0200

    Seventh Commit
    *added educational scene
    *added back button
    *added code to take you to gases screen
    *added code to make educational molecules look animated

---
+++ assignment2/Assets/code/backtomenu3d.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backtomenu3d : MonoBehaviour {
+	RaycastHit hit = new RaycastHit();
+	Ray ray = new Ray();        
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0))  //Returns true during the frame the user touches the object
+		{
+			Debug.Log("mouse down");
+			if (Physics.Raycast (ray, out hit)) 
+			{
+				Debug.Log("pew pew");
+				if (hit.collider.gameObject.name.Equals("menu"))   
+				{
+					Application.LoadLevel(0);
+				}
+			}
+		}
+		else {
+		}
+}
+}
+
+
---
+++ assignment2/Assets/code/rotation3d.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotation3d : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up * 10f * Time.deltaTime);
+	}
+}
