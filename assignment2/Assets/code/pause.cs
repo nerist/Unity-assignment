c2c9272754235cb68529e79497462e71b47d7646
Nicholas Farrugia <Nerist@Nicholass-MacBook-Pro.local>
2015-06-15 00:20:08 +0200

    sixth commit
    *added pause/unpause feature
    *made exit button work
    *fixed start button

---
+++ assignment2/Assets/code/pause.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pause : MonoBehaviour {
+	public GameObject unpauseButton;
+	public AudioClip pausa;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown() {
+		audio.PlayOneShot(pausa);
+		Time.timeScale=0;
+
+			
+			GameObject varGameObject = GameObject.FindWithTag("paddle");
+			
+			varGameObject.GetComponent<paddlecontroller>().enabled = false;
+		Instantiate (unpauseButton, new Vector3 (-0.06f, 0.05f, -2f), Quaternion.Euler (new Vector3 (0, 0, 0)));
+		}
+		}
+	
+
---
+++ assignment2/Assets/code/unpause.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unpause : MonoBehaviour {
+	public AudioClip pausa;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown() {
+
+		Time.timeScale=1;
+		GameObject varGameObject = GameObject.FindWithTag("paddle");
+		audio.PlayOneShot(pausa);
+		
+		varGameObject.GetComponent<paddlecontroller>().enabled = true;
+		Destroy(this.gameObject);
+
+	}
+}
